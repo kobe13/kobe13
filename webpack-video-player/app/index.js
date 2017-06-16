7bf36943baf69497eaa6197c69405e279570160b
kobe13 <kobe13010@gmail.com>
2017-06-16 03:10:43 +0200

    clean up

--- webpack-video-player/app/index.js
+++ webpack-video-player/app/index.js
@@ -1,6 +1,6 @@
-import './main.scss';
-import Video from './video';
-import {playersList} from '../src/js/helpers/player.class';
+import '../src/scss/main.scss';
+import Video from '../src/js/video';
+import { playersList } from '../src/js/helpers/player.class';
 
 playersList.forEach(player => {
   const dataOptions = JSON.parse(player.getAttribute('data-player'));
