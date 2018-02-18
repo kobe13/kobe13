31402d3d4ae48266897da230c79676357df80a04
kobe13 <kobe13010@gmail.com>
2018-02-18 17:19:06 +0100

    refactoring

--- react-js-github-api-test/src/js/components/projects/OrgSearch.js
+++ react-js-github-api-test/src/js/components/projects/OrgSearch.js
@@ -2,33 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const OrgSearch = props => (
-  <div>
-    {/* <h4>Load projects:</h4> */}
-    <form onSubmit={props.submitAction}>
-      <div className="form-group">
-        <label>Load projects:</label>
-        <div className="input-group">
-          <input
-            name="orgName"
-            className="form-control"
-            type="text"
-            value={props.orgName}
-            onChange={props.onChangeAction}
-            placeholder="Enter the org name. e.g. facebook, github, twitter..."
-            required
-          />
-          <span className="input-group-btn">
-            <button
-              type="submit"
-              value="Load projects"
-              className="btn btn-primary">
-              Go!
+  <form onSubmit={props.submitAction}>
+    <div className="form-group">
+      <label htmlFor="orgName">Load projects:</label>
+      <div className="input-group">
+        <input
+          id="orgName"
+          name="orgName"
+          className="form-control"
+          type="text"
+          value={props.orgName}
+          onChange={props.onChangeAction}
+          placeholder="Enter the org name. e.g. facebook, github, twitter..."
+          required
+        />
+        <span className="input-group-btn">
+          <button
+            type="submit"
+            value="Load projects"
+            className="btn btn-primary">
+            Go!
           </button>
-          </span>
-        </div>
+        </span>
       </div>
-    </form>
-  </div>
+    </div>
+  </form>
 );
 
 OrgSearch.propTypes = {
